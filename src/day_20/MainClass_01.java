0e8f74a4d3af263f7ccc823a7d0d122e5270db60
minjeonghwan <ghks7404@naver.com>
2021-03-05 16:43:13 +0900

    team

---
+++ src/day_20/MainClass_01.java
@@ -0,0 +1,25 @@
+package day_20;
+
+import java.util.Scanner;
+
+public class MainClass_01 {
+	public static void main(String[] args) {
+		while(true) {
+			System.out.println("1.더하기 2.빼기 3.곱하기 4.나누기");
+			
+			Scanner sc = new Scanner(System.in);
+			int num = sc.nextInt();
+			
+			switch(num) {
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			}
+		}
+	}
+}
